0b4f62d0d58b00b000455c309ffa8d0bde32796b
qwertimer1 <timothy.dell@dst.defence.gov.au>
2021-02-18 10:09:30 +1100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="tasko", 
+    version="0.0.1",
+    author="qwertimer1",
+    author_email="delltim@gmail.com",
+    description="Tasko package",,
+    packages=setuptools.find_packages(tasko),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
